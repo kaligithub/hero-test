4cd8ecbfa013c1ef9b4b5424f4e83e671091fb94
kcsikarwar <kcsikarwar@gmail.com>
2017-03-24 01:27:22 +0530

    form data binding

--- src/app/hero-form.component.ts
+++ src/app/hero-form.component.ts
@@ -7,4 +7,14 @@ import { Hero }    from './hero';
   templateUrl: './hero-form.component.html'
 })
 export class HeroFormComponent {
+   powers = ['Really Smart', 'Super Flexible',
+             'Super Hot', 'Weather Changer'];
+
+   model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
+   submitted = false;
+
+   onSubmit() { this.submitted = true; }
+
+   
 }
